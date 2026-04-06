a662edb3707bf608bf59d1dee38f3b00271cbf17
mariyam-jasmine <mariumbinthsiddiq@gmail.com>
2026-04-06 20:30:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='', template_folder='')
+
+@app.route('/')
+def home():
+    return send_from_directory('', 'index.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
